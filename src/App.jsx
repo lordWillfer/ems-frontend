b2b057d9d01910b4e9af5dce9ea7827a6130a407
Williams E. Fernández <williamsenrique@gmail.com>
2024-02-22 10:17:04 -0600

    Agregando navegación en la aplicación

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './App.css'
-import HelloWorld from './HelloWorld'
 import FooterComponent from './components/FooterComponent'
 import HeaderComponent from './components/HeaderComponent'
 import ListEmployeeComponent from './components/ListEmployeeComponent'
@@ -8,9 +8,14 @@ function App() {
 
   return (
     <>
-      <HeaderComponent />
-      <ListEmployeeComponent />
-      <FooterComponent />
+      <BrowserRouter>
+        <HeaderComponent />
+        <Routes>
+          <Route path='/' element= {<ListEmployeeComponent />}></Route>
+          <Route path='/employees' element= {<ListEmployeeComponent />}></Route>
+        </Routes>
+        <FooterComponent />
+      </BrowserRouter>
     </>
   )
 }
