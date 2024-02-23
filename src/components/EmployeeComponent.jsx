87a686451694f8c557115f7a33fb07f59bbde590
Williams E. Fernández <williamsenrique@gmail.com>
2024-02-23 17:30:08 -0600

    Cargando los datos del empleado en el formulario de edición

--- src/components/EmployeeComponent.jsx
+++ src/components/EmployeeComponent.jsx
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import { createEmployee } from '../services/EmployeeService';
+import React, { useEffect, useState } from 'react';
+import { createEmployee, getEmployee } from '../services/EmployeeService';
 import { useNavigate, useParams } from 'react-router-dom';
 
 function EmployeeComponent() {
@@ -24,6 +24,18 @@ function EmployeeComponent() {
     }
 
     const navigator = useNavigate();
+
+    useEffect(() => {
+        if (id) {
+            getEmployee(id).then((response) => {
+                setFirstName(response.data.firstName);
+                setLastName(response.data.lastName);
+                setEmail(response.data.email);
+            }).catch(error => {
+                console.error(error);
+            })
+        }
+    }, [id])
 
     function saveEmployee(e) {
         e.preventDefault();
