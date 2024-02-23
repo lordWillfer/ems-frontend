928f1f782e962d984e63ef219e1ffbfa2c9d7ff4
Williams E. Fernández <williamsenrique@gmail.com>
2024-02-22 18:07:43 -0600

    Cambiando el establecimiento de valores mediante funciones flecha

--- src/components/EmployeeComponent.jsx
+++ src/components/EmployeeComponent.jsx
@@ -6,18 +6,6 @@ function EmployeeComponent() {
     const [lastName, setLastName] = useState('')
     const [email, setEmail] = useState('')
 
-    function handleFirstName(e) {
-        setFirstName(e.target.value);
-    }
-
-    function handleLastName(e) {
-        setLastName(e.target.value);
-    }
-
-    function handleEmail(e) {
-        setEmail(e.target.value);
-    }
-
     function saveEmployee(e) {
         e.preventDefault();
 
@@ -35,17 +23,17 @@ function EmployeeComponent() {
                         <div className='form-group mb-2'>
                             <label className='form-label'>First Name:</label>
                             <input type='text' placeholder='Enter Employee First Name' name='firstName' value={firstName}
-                            className='form-control' onChange={handleFirstName} />
+                            className='form-control' onChange={(e) => setFirstName(e.target.value)} />
                         </div>
                         <div className='form-group mb-2'>
                             <label className='form-label'>Last Name:</label>
                             <input type='text' placeholder='Enter Employee Last Name' name='lastName' value={lastName}
-                            className='form-control' onChange={handleLastName} />
+                            className='form-control' onChange={(e) => setLastName(e.target.value)} />
                         </div>
                         <div className='form-group mb-2'>
                             <label className='form-label'>Email:</label>
                             <input type='text' placeholder='Enter Employee Email' name='email' value={email}
-                            className='form-control' onChange={handleEmail} />
+                            className='form-control' onChange={(e) => setEmail(e.target.value)} />
                         </div>
 
                         <button className='btn btn-success' onClick={saveEmployee}>Submit</button>
